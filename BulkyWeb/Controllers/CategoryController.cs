e23382fa20ab90073b66c0801d7d2d97ec820df8
LanAd2 <tranthanhlan.ad2@gmail.com>
2023-07-06 16:49:12 +0700

    Implement the update functionalilty of a category.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,5 +37,38 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
