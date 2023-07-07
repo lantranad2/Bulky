70a0aa24babe6356e648c46576114cd99f0ddeb5
LanAd2 <tranthanhlan.ad2@gmail.com>
2023-07-07 17:22:02 +0700

    Display all categories in a page 'Index'.

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category>? Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db) => _db = db;
+
         public void OnGet()
         {
+            Categories = _db.Categories.ToList();
         }
     }
 }
